7c571bb9b8c5e8df050d0e110c650296740bc280
brilliantputritegar <brilliantputritegar@gmail.com>
2022-06-13 21:33:38 +0700

    assets colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,30 @@ module.exports = {
         extend: {
             colors: {
                 primary: {
-                    darkblue05: "#4B1979",
+                    purple05: "#4B1979",
+                    purple04: "#7126B5",
+                    purple03: "#A06ECE",
+                    purple02: "#D0B7E6",
+                    purple01: "#E2D4F0",
+
+                    cream05: "#AA9B87",
+                    cream04: "#D4C2A8",
+                    cream03: "#FFE9CA",
+                    cream02: "#FFF0DC",
+                    cream01: "#FFF8ED"
+
+                },
+                allert: {
+                    danger: "#FA2C5A",
+                    warning: "#F9CC00",
+                    succes: "#73CA5C"
+                },
+                neutral:{
+                    neutral05: "#151515",
+                    neutral04: "#3C3C3C",
+                    neutral03: "#8A8A8A",
+                    neutral02: "#D0D0D0",
+                    neutral01: "#FFFFFF"
                 },
             },
         },
