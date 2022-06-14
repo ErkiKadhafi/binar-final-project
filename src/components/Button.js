78b1c6355e0e8b034c304a57da18910a68b39e3a
ErkiKadhafi <erki.qaedhafi@gmail.com>
2022-06-14 19:27:36 +0700

    fix button alignment

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,7 +11,7 @@ const Button = ({
             ? "bg-primary-darkblue04 hover:bg-primary-darkblue05  text-white focus:ring-primary-darkblue04 focus:ring-offset-primary-darkblue03 focus:ring-2 focus:ring-offset-2"
             : "bg-white hover:bg-gray-300 border border-primary-darkblue04 focus:bg-gray-300";
     const customClass =
-        " transition ease-in duration-200 text-center text-sm shadow-md focus:outline-none font-medium rounded-xl";
+        "flex items-center justify-center transition ease-in duration-200 text-center text-sm shadow-md focus:outline-none font-medium rounded-xl";
     return (
         <button
             className={`${sizeClass} ${colorClass} ${customClass} ${className}`}
