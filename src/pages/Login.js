6a09ec14b0bff2e0a800da887a225c5ca13d3f10
AndieGagas <andiealfriant01@gmail.com>
2022-06-14 22:32:03 +0700

    finish UI  login n register page

---
+++ src/pages/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Input from "../components/Input";
+import Button from "../components/Button";
+
+const Login = () => {
+
+    return(
+        <section className="h-screen mx-4 font-poppins md:grid md:grid-cols-2 md:mx-0">
+            <img src="/images/banner-login.svg" alt="" className="banner-login w-full h-screen object-cover" />
+            <div className="space-y-6 md:flex md:flex-col md:w-3/5 md:m-auto">
+                <header className='py-4 md:hidden'>
+                    <img src="/images/fi_arrow-left.svg" alt=''/>
+                </header>
+                <h2 className='text-2xl font-bold'>Masuk</h2>
+                <form>
+                    <fieldset className='flex flex-col gap-1'>
+                        <label>Email</label>
+                        <Input type="email" placeholder="Contoh: johndee@gmail.com"/>
+                    </fieldset>
+                    <fieldset className='flex flex-col gap-1 mt-4'>
+                        <label>Password</label>
+                        <Input type="password" placeholder="Masukkan password" />
+                    </fieldset>
+                    <Button className="w-full mt-6" >Masuk</Button>
+                </form>
+                <p className="text-center">Belum punya akun?
+                    <a href="/register" className="text-primary-darkblue04 font-bold ml-2">Daftar di sini</a>
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Login
