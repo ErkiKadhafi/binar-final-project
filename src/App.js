7017606f8b86c129cb1d5d75083d73e30b0e45cd
ErkiKadhafi <erki.qaedhafi@gmail.com>
2022-06-15 22:08:50 +0700

    finish halaman product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />}></Route>
+                <Route path="/product/:id" element={<Product />}></Route>
             </Routes>
         </BrowserRouter>
     );
